bd56975db836effadc73ba00b2d93ca978a92d7b
Rishabh Kumar <rishabhkumar1211@gmail.com>
2024-11-10 00:15:57 +0530

    correction of inital seed

--- seedData.js
+++ seedData.js
@@ -1,4 +1,3 @@
-// seedData.js
 const sequelize = require("./database");
 const VehicleType = require("./models/VehicleType");
 const Vehicle = require("./models/Vehicle");
@@ -6,16 +5,29 @@ const Vehicle = require("./models/Vehicle");
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
 
-  const carType = await VehicleType.create({ name: "Car", wheels: 4 });
-  const bikeType = await VehicleType.create({ name: "Bike", wheels: 2 });
+  // Seed Car Types with realistic models
+  const hatchbackType = await VehicleType.create({
+    name: "Hatchback",
+    wheels: 4,
+  });
+  const suvType = await VehicleType.create({ name: "SUV", wheels: 4 });
+  const sedanType = await VehicleType.create({ name: "Sedan", wheels: 4 });
 
-  await Vehicle.create({ model: "Hatchback", typeId: carType.id });
-  await Vehicle.create({ model: "SUV", typeId: carType.id });
-  await Vehicle.create({ model: "Sedan", typeId: carType.id });
-  await Vehicle.create({ model: "Cruiser", typeId: bikeType.id });
-  await Vehicle.create({ model: "Sports", typeId: bikeType.id });
+  // Seed Bike Types with realistic models
+  const cruiserType = await VehicleType.create({ name: "Cruiser", wheels: 2 });
+  const sportsType = await VehicleType.create({ name: "Sports", wheels: 2 });
 
-  console.log("Database seeded successfully.");
+  // Associate Vehicles with Types
+  await Vehicle.create({ model: "Toyota Yaris", typeId: hatchbackType.id });
+  await Vehicle.create({ model: "Ford EcoSport", typeId: suvType.id });
+  await Vehicle.create({ model: "Honda Accord", typeId: sedanType.id });
+  await Vehicle.create({
+    model: "Harley Davidson Street 750",
+    typeId: cruiserType.id,
+  });
+  await Vehicle.create({ model: "Yamaha YZF R1", typeId: sportsType.id });
+
+  console.log("Database seeded successfully with realistic models.");
 };
 
 seedDatabase().catch(console.error);
